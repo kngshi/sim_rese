252f2364758b4b14944ce3e1b9db77b02aab47e2
KoheiNegishi <nk.0910.shanti@gmail.com>
2024-05-03 21:17:39 +0900

    add: シーダーファイルの作成

---
+++ src/database/seeders/AreasTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class areasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $param = [
+        'id' => '1',
+        'name' => '東京都',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+        'id' => '2',
+        'name' => '大阪府',
+        ];
+        DB::table('areas')->insert($param);
+
+        $param = [
+        'id' => '3',
+        'name' => '福岡県',
+        ];
+        DB::table('areas')->insert($param);
+
+    }
+}
