cb957494a81509fd952f3a6d7b560cf60c754e98
KoheiNegishi <nk.0910.shanti@gmail.com>
2024-05-03 20:02:26 +0900

    delete: 不要なマイグレーションファイルの削除

--- src/database/migrations/2024_05_03_182057_create_favorites_table.php
+++ src/database/migrations/2024_05_03_182057_create_favorites_table.php
@@ -15,7 +15,12 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('shop_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
         });
     }
 
